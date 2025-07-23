15a1763fe8b780575a0cbb6f9de6b6d85909b290
Aleksey Alekseev <alexseev617@gmail.com>
2025-07-23 19:27:45 +0300

    Добавил глобальные стили

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+    font-family: 'Kanit', Arial;
+    font-weight: 500;
+    color: #252525;
+  }
+`
